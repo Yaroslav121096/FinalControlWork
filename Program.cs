b94f3e4f0fa3f08a82c1e99dd669877ad3932b02
Yaroslav <Valso96@mail.ru>
2022-12-25 21:41:54 +0600

    Добавление кода в файл Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Введите значение {i + 1} строки: ");
+    array[i] = Console.ReadLine();
+    if (array[i].Length <= 3)
+        count++;
+}
+string[] result = new string[count];
+int j = 0;
+int a = 0;
+while (j < n)
+    if (array[j].Length <= 3)
+    {
+        result[a] = array[j];
+        a++;
+        j++;
+    }
+    else
+        j++;
+Console.Clear();
+Console.WriteLine($"Ваш массив:\n{string.Join(", ", array)}");
+Console.WriteLine($"\nЭлементы массива, длина которых меньше, либо равна 3:\n{string.Join(", ", result)}");
